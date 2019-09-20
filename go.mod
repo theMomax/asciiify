55adf173ece5f60174aec010c2a2e126e933eada
veecue <veecue@ventos.tk>
2019-09-20 23:17:00 +0200

    add sensible player

--- go.mod
+++ go.mod
@@ -4,10 +4,11 @@ go 1.12
 
 require (
 	github.com/aybabtme/rgbterm v0.0.0-20170906152045-cc83f3b3ce59 // indirect
-	github.com/buger/goterm v0.0.0-20181115115552-c206103e1f37
 	github.com/mattn/go-isatty v0.0.9 // indirect
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
-	github.com/pkg/profile v1.3.0
 	github.com/qeesung/image2ascii v1.0.1
+	github.com/rthornton128/goncurses v0.0.0-20190619232326-514c399d6652
+	github.com/stretchr/testify v1.4.0 // indirect
+	github.com/tomnomnom/xtermcolor v0.0.0-20160428124646-b78803f00a7e
 	github.com/wayneashleyberry/terminal-dimensions v1.0.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,29 +1,76 @@
 package main
 
 import (
-	tm "github.com/buger/goterm"
+	"image/color"
+	"image/gif"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/rthornton128/goncurses"
+	"github.com/theMomax/asciiify/asciiif"
+	"github.com/tomnomnom/xtermcolor"
 )
 
 func main() {
-	tm.Println()
+	if len(os.Args) == 0 {
+		log.Println("Self recursion!")
+	}
 
-	// tm.GetXY()
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	go func() {
+		for range sigs {
 
-	// // tm.Clear()
+		}
+	}()
 
-	// asciiif, err := g.FromFile(os.Args[1], &convertOptions)
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
+	stdscr, err := goncurses.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer goncurses.End()
 
-	// for _, i := range asciiif.Image {
-	// 	tm.MoveCursor(1, h)
+	goncurses.Cursor(0)
+	stdscr.ScrollOk(false)
 
-	// 	tm.Println(i)
+	gifFile, err := os.Open(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+	defer gifFile.Close()
 
-	// 	tm.Flush()
+	gif, err := gif.DecodeAll(gifFile)
+	if err != nil {
+		panic(err)
+	}
 
-	// 	time.Sleep(time.Second)
-	// }
+	vid := asciiif.DecodeGIFStreamed(gif)
+
+	goncurses.StartColor()
+
+	for i := int16(0); i < 256; i++ {
+		goncurses.InitPair(i, i, 0)
+	}
+
+	for img := range vid {
+		for y, row := range img.Image {
+			for x, pix := range row {
+				stdscr.ColorOn(int16(xtermcolor.FromColor(color.RGBA{pix.R, pix.G, pix.B, pix.A})))
+				stdscr.MoveAddChar(y, x, goncurses.Char(pix.Char))
+			}
+		}
+		stdscr.Refresh()
+		time.Sleep(time.Duration(img.Delay) * 10 * time.Millisecond)
+	}
+
+	/*
+		fmt.Println(os.Args)
+		if len(os.Args) > 1 {
+			syscall.Exec(os.Args[0], os.Args[1:], os.Environ())
+		} else {
+			syscall.Exec(os.Args[0], []string{}, os.Environ())
+		}*/
 }
